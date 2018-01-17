0bc013bd595428fcb16a89b461ca32406d7028b4
Hong Sung <profsung@gmail.com>
2018-01-16 20:28:36 -0600

    class_ex1 revised

--- class_ex1.js
+++ class_ex1.js
@@ -9,15 +9,19 @@ function car (mpg) {
 }
 
 let c1 = new car(10);
-console.log(c1.odometer, c1.gas, c1.mpg);
+console.log('c1=', c1.odometer, c1.gas, c1.mpg);
 c1.gas = 5;
 c1.drive(20);
-console.log(c1.odometer, c1.gas, c1.mpg);
+console.log('c1=', c1.odometer, c1.gas, c1.mpg);
 car.prototype.make = null; // adding a new property
 let c2 = new car(35);
 c2.make = 'Ford';
-console.log(c2.odometer, c2.gas, c2.mpg, c2.make);
+c2.gas = 100;
+c2.drive(100);
+console.log('c2=', c2.odometer, c2.gas, c2.mpg, c2.make);
 car.prototype.reset = function ( ) {this.odometer = 0;}
 c1.make = 'GM';
 c1.reset();
-console.log(c1.odometer, c1.gas, c1.mpg, c1.make);
+console.log('c1=',c1.odometer, c1.gas, c1.mpg, c1.make);
+c2.reset();
+console.log('c2=',c2.odometer, c2.gas, c2.mpg, c2.make);
