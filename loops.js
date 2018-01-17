fb805de5806cbe3b0bd1bbf47dc531e282fbd219
Hong Sung <profsung@gmail.com>
2018-01-16 20:10:50 -0600

    loops added

---
+++ loops.js
@@ -0,0 +1,27 @@
+let a = [10, 20, 'pen', 'car', {id: 12, name: 'John'}];
+
+let i = 0;
+while (i < a.length) {
+    console.log(a[i++]);
+}
+
+let n = 0;
+do {
+    console.log(a[n++]);
+} while (n < a.length);
+
+for (let p = 0; p < a.length; p++) {
+    console.log(a[p]);
+}
+
+for (const c of a) {
+    console.log(c);
+}
+
+for (const c in a) {
+    console.log(c, a[c]);
+}
+
+for (const [index, element] of a.entries()) {
+    console.log(index, element, `type = ${typeof element}`);
+}
