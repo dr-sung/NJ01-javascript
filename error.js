345078ba2bac57b4b67ceeb8eebf546932fbf876
Hong Sung <profsung@gmail.com>
2018-01-17 20:12:56 -0600

    error.js revised

--- error.js
+++ error.js
@@ -7,3 +7,11 @@ try {
 } catch (e) {
     console.log("An error is caught: " + e.message);
 }
+
+try {
+    console.log('started');
+    throw 'error message';
+    console.log('can you see this message?');
+} catch (e) {
+    console.log(e);
+}
