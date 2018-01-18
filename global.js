eb953dfafeeca6fbc634b5b6583a146e59240d59
Hong Sung <profsung@gmail.com>
2018-01-17 20:15:24 -0600

    global.js revised

--- global.js
+++ global.js
@@ -1,6 +1,10 @@
 console.time("m1");
 global.name = "Cool app!";
+
+console.time("m2");
 console.log(global.name);
 console.assert(global.name == "Cool app!");
 console.warn("this is a warning message");
+console.timeEnd("m2");
+
 console.timeEnd("m1");
